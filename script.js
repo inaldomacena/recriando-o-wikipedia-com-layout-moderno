0d66dcd052060c99e2a37d26bc197bff70aa0031
Inaldo Macena <inaldomacena19@gmail.com>
2023-08-25 02:24:51 -0300

    Subindo projeto do curso da Dio.me

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+
+function clickMenu(){
+  if(itens.style.display == 'none'){
+     itens.style.display =  'block'
+  }else{
+    itens.style.display = 'none'
+  }
+  
+}
+
+
+
+/* <nav class="menu"id="itens" onclick="clickMenu()">
+        
+        <i class="material-icons">
+            menu
+        </i>
+
+        <ul>
+          <li><a href="#">Página principal</a></li>
+          <li><a href="#"> Conteudo destacado</a></li>
+          <li><a href="#">Evento atuais</a></li>
+        </ul>
+      </nav> */
